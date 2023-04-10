d8c151c90496acf5837bbef630d5f0d272fd3751
Fadimo Mahamud <fadimomahamud@Fatimas-MacBook-Air.local>
2023-04-10 17:12:51 +0100

    lesson 1 javascript

---
+++ Lesson/Javascript/lesson1.js
@@ -0,0 +1,21 @@
+//How to print off a string
+console.log("This is javascript lesson 1");
+
+
+//variables
+let age = 25;
+
+const name = "Fatima";
+
+console.log(age+" "+name);
+
+
+
+
+
+
+
+
+
+
+
